407490c19b2e95ee7909e3e8dc62fdb6efbc576c
Ricardo Lopes <ricardo.lopes@ifood.com.br>
2022-06-08 11:40:03 -0300

    Add chalk as dependecy

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello node");
+import chalk from 'chalk';
+
+console.log(chalk.blue.bgWhite.bold("Hello Node"));
+
+console.log(`
+ CPU: ${chalk.red('90%')}
+ RAM: ${chalk.green('40%')}
+ DISK: ${chalk.yellow('70%')}   
+`);
