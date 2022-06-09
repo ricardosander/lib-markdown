6a848ba65603ca5917d79055e42c7261fef21b3f
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2022-06-09 00:33:08 -0300

    Reading a file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import chalk from 'chalk';
+import fs from 'fs';
 
 console.log(chalk.blue.bgWhite.bold("Hello Node"));
 
@@ -7,3 +8,17 @@ console.log(`
  RAM: ${chalk.green('40%')}
  DISK: ${chalk.yellow('70%')}   
 `);
+
+async function getFile(filePath) {
+    const enconding = 'utf-8';
+    try {
+        const text = await fs.promises.readFile(filePath, enconding)
+        console.log(chalk.green(text));
+    } catch (error) {
+        throw new Error(chalk.red(error.code, 'File not found'));
+    } finally {
+        console.log(chalk.yellow('Operation finished'));
+    }
+}
+
+getFile('./package.json');
