28680f27496fa772d3ed9cda59fec4a68b5c8029
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2022-06-11 20:22:08 -0300

    Create link validator.

---
+++ http-validator.js
@@ -0,0 +1,28 @@
+import fetch from "node-fetch";
+
+async function validateLinks(links) {
+    console.log('Validating links...');
+    const results = await Promise.all(links.map(link => { 
+        return validate(link).then(
+            status => {
+                 console.log(`Status ${status} for ${link.link} from ${link.file}`);
+                return {
+                    ...link,
+                    ...status
+                };
+            }
+        );
+    }));
+    console.log(results);
+}
+
+async function validate(link) {
+    console.log(`Validating ${link.link} from ${link.file}`);
+    const result = await fetch(link.link);
+    return {
+        "status": result.status,
+        "response": result.status
+    };
+}
+
+export default validateLinks;
