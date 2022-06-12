a1b4fcf48e01dbbbf3bd1e235dba9b934671af64
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2022-06-11 21:48:01 -0300

    Make project compatible with jest

--- cli.js
+++ cli.js
@@ -1,16 +1,19 @@
-import chalk from "chalk";
-import getFile from "./index.js";
-import validateLinks from "./http-validator.js";
+const chalk = require('chalk');
+const getFile = require('./index.js');
+const validateLinks = require('./http-validator.js');
 
 const args = process.argv;
 
 if (args.length < 3) {
     console.log(chalk.red('File or directory is required.'));
 } else {
-
     const validate = process.argv.findIndex(s => s === 'validate') !== -1;
-
     const path = args[2];
+
+    exec(path, validate);
+}
+
+async function exec(path, validate) {
     const links = await processText(path);
     if (validate) {
         validateLinks(links);
@@ -19,6 +22,6 @@ if (args.length < 3) {
     }
 }
 
-function processText(path) {
+async function processText(path) {
     return getFile(path);
 }
