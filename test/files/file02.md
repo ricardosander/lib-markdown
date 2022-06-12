c86ba2f3949306aff83faba12fb01a64f3e4bc0f
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2022-06-11 23:43:07 -0300

    Add basic unit tests

---
+++ test/files/file02.md
@@ -0,0 +1,3 @@
+A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo.
+
+ Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)
--- test/index.test.js
+++ test/index.test.js
@@ -6,8 +6,28 @@ describe('getFile::', () => {
         expect(typeof getFile).toBe('function');
     });
 
-    it('should return an array with results', async () => {
-        const result = await getFile('files');
-        expect(result.length).toBe(10);
+    it('should return an empty array when given file is empty', async () => {
+        const result = await getFile('test/files/empty.md');
+        expect(result.length).toBe(0);
+    });
+
+    it('should return an empty array when given file has no links', async () => {
+        const result = await getFile('test/files/no-links.md');
+        expect(result.length).toBe(0);
+    });
+
+    it('should return 4 links when given file is file01.md', async () => {
+        const result = await getFile('test/files/file01.md');
+        expect(result.length).toBe(4);
+    });
+
+    it('should return 1 links when given file is file02.md', async () => {
+        const result = await getFile('test/files/file02.md');
+        expect(result.length).toBe(1);
+    });
+
+    it('should return 5 links when directory is given', async () => {
+        const result = await getFile('test/files/');
+        expect(result.length).toBe(5);
     });
 });
